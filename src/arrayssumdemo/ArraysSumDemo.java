28dbd1d934049112114eee4b8cffff99bea388cf
freegroup4 <freegroup4@yopmail.com>
2014-05-24 23:45:14 +0800

    Second Commit
    Second Commit

--- src/arrayssumdemo/ArraysSumDemo.java
+++ src/arrayssumdemo/ArraysSumDemo.java
@@ -1,6 +1,11 @@
 package arrayssumdemo;
 public class ArraysSumDemo {
     public static void main(String[] args) {
-        int arr[] = 
+        int arr[] = {15,8,6,7,24};
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) 
+            sum += arr[i];
+            System.out.println("sum=" + sum);
+            System.out.println("平均值=" + (float)sum/arr.length);
     }
 }
